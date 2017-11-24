525744370a63c26189b08a7fdf8af49b91fef0c5
Michiel Overeem <overeemm@gmail.com>
2017-11-24 16:49:12 +0100

    Added approach to sanitizing the negatives

--- Kata011.Calculator/Calculator.fs
+++ Kata011.Calculator/Calculator.fs
@@ -14,10 +14,16 @@ let add =
   let map = Array.map
   let fold = Array.fold
 
-  let failOnNonEmptyListWithEmptyValues (array : string[]) : string[] = 
+  let sanitizeInput (array : string[]) : string[] = 
                          match array.Length with
                             | 0  -> array
                             | 1  -> array
                             | _  -> map failOnEmpty array
 
-  split [|',';'\n'|] >> failOnNonEmptyListWithEmptyValues >> map parse >> fold (+) 0
+  //let sanitizeNegatives = 
+  //  let collect (ns,ps)= match nr < 0 with
+  //                  | true  -> (nr:ns,ps)
+  //                  | false -> (ns   ,pr:ps)
+  //  fold collect ([], [])
+
+  split [|',';'\n'|] >> sanitizeInput >> map parse >> fold (+) 0
