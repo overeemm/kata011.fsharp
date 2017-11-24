27e76e00a4e3b1a9fb66aca4c3a97f58cc0b860d
Michiel Overeem <overeemm@gmail.com>
2017-11-24 14:57:45 +0100

    Fixed a bug

--- Kata011.Calculator.Tests/CalculatorTest.fs
+++ Kata011.Calculator.Tests/CalculatorTest.fs
@@ -7,3 +7,11 @@ open CalculatorFunctions
 [<Test>]
 let ``with simple number should return the number`` () =
   1 |>  should equal (add "1")
+
+[<Test>]
+let ``with empty string should return 0`` () =
+  0 |>  should equal (add "")
+
+[<Test>]
+let ``with multiple numbers should return addition`` () =
+  3 |>  should equal (add "1,2")
--- Kata011.Calculator/Calculator.fs
+++ Kata011.Calculator/Calculator.fs
@@ -2,6 +2,9 @@
 
 let add = 
   let split seperator (text : string) = text.Split seperator
-  let parse (text : string) = System.Int32.Parse(text)
-    
+  
+  let parse (text : string) = match System.Int32.TryParse(text) with
+                                | (true,int) -> int
+                                | _          -> 0
+
   split [|','|] >> Array.fold (fun acc x -> acc + parse x) 0
